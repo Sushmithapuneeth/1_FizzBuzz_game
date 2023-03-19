b80baa8a0a8287e5654a2df95ce539cdaeec893b
Sushmithapuneeth <sushmithashreekanth26@gmail.com>
2023-03-19 10:17:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Fizz buzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz", and any number divisible by both 3 and 5 with the word "fizzbuzz".
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("buzz")
+    else:
+        print(number)
